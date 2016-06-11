21cca9318a11436dc2a737d3f11ffc4dcbbba8c4
James Lee <jameleetutorial@gmail.com>
2016-06-11 01:02:02 +0100

    introduce redis

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:3.5
-RUN pip install Flask==0.11.1
+RUN pip install Flask==0.11.1 redis==2.10.5
 RUN useradd -ms /bin/bash admin
 USER admin
 WORKDIR /app
--- app/app.py
+++ app/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, render_template
+import redis
 
 app = Flask(__name__)
 default_key = '1'
-cache = {default_key: 'one'}
+cache = redis.StrictRedis(host='redis', port=6379, db=0)
+cache.set(default_key, "one")
 
 @app.route('/', methods=['GET', 'POST'])
 def mainpage():
@@ -12,11 +14,11 @@ def mainpage():
 	    key = request.form['key']
 
 	if request.method == 'POST' and request.form['submit'] == 'save':
-		cache[key] = request.form['cache_value']
+		cache.set(key, request.form['cache_value'])
 
 	cache_value = None;
-	if key in cache:
-		cache_value = cache[key]
+	if cache.get(key):
+		cache_value = cache.get(key).decode('utf-8')
 
 	return render_template('index.html', key=key, cache_value=cache_value)
 
